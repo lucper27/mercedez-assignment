df44a12edcf35021b0ea80e47e5a941816142ee2
Lucas <lucasperuchena.27@gmail.com>
2025-07-22 07:10:05 +0200

    feat (backend): error dto
    Error Response DTO class created

--- api/adapter/src/main/java/com/swapi/adapter/application/dto/ErrorResponse.java
+++ api/adapter/src/main/java/com/swapi/adapter/application/dto/ErrorResponse.java
@@ -1,4 +1,50 @@
 package com.swapi.adapter.application.dto;
 
+import java.time.Instant;
+
 public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private Instant timestamp;
+    private String path;
+
+    public ErrorResponse(int status, String message, Instant timestamp, String path) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+        this.path = path;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
